8174083f6b910decde218f0477bbcadf9bb85953
mmdhpo <mmdhabibpur@gmail.com>
2024-03-04 01:41:21 +0330

    assignment RegEx completed

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -50,7 +50,6 @@ public class Exercise3 {
 
         return wordsWithRepeatLetters;
     }
-
     private static boolean hasRepeatedLetters(String word) {
         for (int i = 0; i < word.length() - 1; i++) {
             for(int j = i + 1; j < word.length(); j++) {
